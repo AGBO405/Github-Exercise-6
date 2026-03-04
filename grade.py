b52a75b54b565ee8b06de8a165aea9051a556781
Aiden <aibenchibba@gmail.com>
2026-03-03 20:37:36 -0500

    Add grade calculation based on user input

--- grade.py
+++ grade.py
@@ -1,1 +1,18 @@
+name = input("What is your name: ")
 
+score = float(input("Enter your grade 0-100: "))
+
+
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print(f"{name}, your grade is an {grade}")
+                
